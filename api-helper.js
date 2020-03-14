55f8684ab4c795dcefb038b8cf86e7c0a8caacb4
YashJipkate <yashjipkate@gmail.com>
2020-03-14 20:09:05 +0530

    Completed all endpoints

--- api-helper.js
+++ api-helper.js
@@ -6,10 +6,11 @@ module.exports = {
     ** which gets resolved or rejected based
     ** on the result from the API
     */
-    makeApiCall : function(url) {
+    makeApiCall : function(url, starting_after) {
+        const queryString = ((starting_after==='null' || typeof starting_after==='undefined') ? '' : '?starting_after=' + starting_after);
         return new Promise((resolve, reject) => {
             request({
-                url: url,
+                url: url + queryString,
                 headers: {Authorization: 'Bearer ' + process.env.AUTH_TOKEN}},
                 (err, res, body) => {
                     if (err) reject(err)
--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
+var cors = require('cors')
 const app = express()
 const port = 3000
+
+app.use(cors())
 
 const apiHelper = require('./api-helper')
 
@@ -9,7 +12,27 @@ require('dotenv').config();
 app.get('/', (req, res) => res.send('Home page!'))
 
 app.get('/people', (req, res) => {
-    apiHelper.makeApiCall('https://api.zenefits.com/core/people')
+    apiHelper.makeApiCall('https://api.zenefits.com/core/people', req.query.starting_after)
+    .then(response => {
+        res.send(response)
+    })
+    .catch(error => {
+        res.send(error)
+    });
+})
+
+app.get('/departments', (req, res) => {
+    apiHelper.makeApiCall('https://api.zenefits.com/core/departments', req.query.starting_after)
+    .then(response => {
+        res.send(response)
+    })
+    .catch(error => {
+        res.send(error)
+    });
+})
+
+app.get('/companies', (req, res) => {
+    apiHelper.makeApiCall('https://api.zenefits.com/core/companies', req.query.starting_after)
     .then(response => {
         res.send(response)
     })
